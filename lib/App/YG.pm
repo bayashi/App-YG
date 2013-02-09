51291c8fd5f4a67ee4adda746cf641f2a2a154d4
bayashi <bayashi@cpan.org>
2013-02-09 14:15:00 +0900

    bumped version

--- lib/App/YG.pm
+++ lib/App/YG.pm
@@ -7,7 +7,7 @@ use Pod::Usage;
 use IO::Interactive qw/is_interactive/;
 use Term::ANSIColor qw/colored/;
 
-our $VERSION = '0.041';
+our $VERSION = '0.05';
 
 our $CONFIG_FILE    = '.ygconfig';
 our $DEFAULT_PARSER = 'apache-combined';
